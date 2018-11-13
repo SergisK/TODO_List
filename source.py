767448e2d89027de0b17b022c159daee4f86118d
SergisK <sergiusz.kanski@gmail.com>
2018-11-13 17:45:19 +0100

    Clear main file

--- source.py
+++ source.py
@@ -1,14 +1,5 @@
 from tkinter import *
 
 window = Tk()
-label=Label(window, text='TEKST')
 
-
-def hello():
-    label['text']='inny'
-
-
-btn = Button(window, text='Click',command=hello)
-btn.pack()
-label.pack()
 window.mainloop()
